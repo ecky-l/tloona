e7a750856a213d46e1b39bc2cd0b09e0660e640e
ecky-l@de0d828f-f52b-0410-b448-d22600a15610 <ecky-l@de0d828f-f52b-0410-b448-d22600a15610>
2008-04-18 23:24:08 +0000

    patch for slaveconsole
    git-svn-id: svn+ssh://svn.code.sf.net/p/tloona/code/tloona/trunk@128 de0d828f-f52b-0410-b448-d22600a15610

---
+++ AppMain.tcl
@@ -0,0 +1,8 @@
+if {[string first "-psn" [lindex $argv 0]] == 0} {
+    set argv [lrange $argv 1 end]
+}
+console show
+if [catch {source [file join [file dirname [info script]] main.tcl]}] {
+    puts $errorInfo
+}
+
