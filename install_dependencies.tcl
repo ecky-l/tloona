bda98ae4a0bed0138cb14bae0e67038446efa701
ecky-l@de0d828f-f52b-0410-b448-d22600a15610 <ecky-l@de0d828f-f52b-0410-b448-d22600a15610>
2013-06-19 07:15:59 +0000

    test
    git-svn-id: svn+ssh://svn.code.sf.net/p/tloona/code/tloona/trunk@178 de0d828f-f52b-0410-b448-d22600a15610

--- install_dependencies.tcl
+++ install_dependencies.tcl
@@ -18,9 +18,27 @@
 # ?      src/snit
 #
 # If you want to create a starpack or starkit, include these directories
+#
+# Unfortunately the excellent sugar macro package is not available via
+# teacup. Please download it from http://wiki.tcl.tk/11155 and install it
+# manually. Alternatively you can copy the directory src/sugar0.1 somewhere
+# to your tcl_pkgPath (execute "set tcl_pkgPath" in a tclsh to find out
+# where your tclsh setup resolves packages)
 
 foreach pkg {comm Img Itcl Itk log htmlparse ctext fileutil tdom starkit vfs::mk4 mk4vfs Tclx} {
     puts "Installing $pkg ..."
     puts [exec teacup install $pkg]
 }
 
+if {[catch package require sugar]} {
+    puts {
+
+
+# Unfortunately the excellent sugar macro package is not available via
+# teacup. Please download it from http://wiki.tcl.tk/11155 and install it
+# manually. Alternatively you can copy the directory src/sugar0.1 somewhere
+# to your tcl_pkgPath (execute "set tcl_pkgPath" in a tclsh to find out
+# where your tclsh setup resolves packages)
+
+    }
+}
