02a68eb32d2de7f6a5d3f6f54165a5769581ec62
elehmann <elehmann>
2016-04-30 12:43:35 +0000

    started new console functionality to use comm remote interpreters

--- src/tmw-console/console.tcl
+++ src/tmw-console/console.tcl
@@ -1,6 +1,6 @@
 # need to be commented
 
-#lappend auto_path src lib
+lappend auto_path src lib
 
 package require snit 2.3.2
 package require tmw::vitext 1.0
@@ -39,6 +39,9 @@ snit::widget ::Tmw::Console {
     
     option {-mode mode Mode} -default n -configuremethod ConfigMode
     variable SlaveInterp {}
+    
+    ## \brief The communications ID if mode is comm
+    variable CommInterp {}
     
     ## \brief The text window to type in
     component textwin -inherit yes
@@ -125,11 +128,31 @@ snit::widget ::Tmw::Console {
         
         grid $hscroll -row 1 -column 0 -padx 0 -pady 0 -sticky wes
         grid $vscroll -row 0 -column 1 -padx 0 -pady 0 -sticky nes
-        $self InsertPrompt
         
-        if {![dict exist $args -mode]} {
+        set promptFcn InsertPrompt
+        if {[dict exists $args -mode]} {
+            set m [dict get $args -mode]
+            if {[lindex $m 0] eq "comm"} {
+                package require comm
+                if {[llength $m] == 1} {
+                    set promptFcn AskForCommInterp
+                }
+            }
+        } else {
             lappend args -mode default
         }
+        $self $promptFcn
+        
+        #if {![dict exist $args -mode]} {
+        #    lappend args -mode default
+        #    $self InsertPrompt
+        #} elseif {[lindex [dict get $args -mode] 0] eq "comm" 
+        #    && [llength [dict get $args -mode]] == 1} {
+        #    $self AskForCommInterp
+        #} else {
+        #    $self InsertPrompt
+        #}
+        
         $self configurelist $args
         
         # do this tag configurations later so that
@@ -197,6 +220,13 @@ snit::widget ::Tmw::Console {
             set err [catch {$SlaveInterp eval $cmd} result]
             if {$err} {
                 set errInfo [$SlaveInterp eval {set errorInfo}] 
+            }
+        }
+        comm {
+            set err [catch {comm::comm send $CommInterp $cmd} result]
+            if {$err} {
+                global __ConsCommResult__
+                set errInfo [dict get $__ConsCommResult__ -errorinfo] 
             }
         }
         default {
@@ -314,19 +344,29 @@ snit::widget ::Tmw::Console {
         set command [$textwin get limit end]
         
         $textwin fastinsert insert "\n"
+        if {$command == "\n" && [$self cget -vimode]} {
+            $self setInsertMode
+        }
+        
+        if {$options(-mode) eq "comm" && $CommInterp == {}} {
+            set command [string trim $command]
+            if {$command != {} && [string is int $command]} {
+                $self SetCommInterp $command
+                $textwin mark set limit insert
+                puts "Connected to Comm ID $command"
+                $self InsertPrompt
+                return
+            }
+            $self AskForCommInterp
+            return
+        }
+        
         if {[info complete $command]} {
             $textwin mark set limit insert
             $self eval $command
         }
         
         set HistLevel -1
-        if {$command == "\n"} {
-            if {[$self cget -vimode]} {
-                $self setInsertMode
-            }
-            return
-        }
-        
         set command [string trimright $command \n]
         set i [lsearch $options(-history) $command]
         if {[set i [lsearch $options(-history) $command]] >= 0} {
@@ -339,6 +379,50 @@ snit::widget ::Tmw::Console {
             set options(-history) \
                 [lrange $options(-history) 0 $options(-maxhistory)]
         }
+    }
+    
+    ## \brief set the comm interp for remote interaction
+    method SetCommInterp {id} {
+        ::comm::comm connect $id
+        ::comm::comm send $id list
+        set CommInterp $id
+        
+        ::comm::comm hook reply {
+            global __ConsCommResult__
+            set __ConsCommResult__ [array get return]
+            dict set __ConsCommResult__ -ret $ret
+        }
+        
+        set script {
+        ::comm::comm hook eval {
+            set cmdl [string trimright [lindex $buffer 0] \n]
+            set cmdArgs [lrange $cmdl 1 end]
+            switch -- [lindex $cmdl 0] {
+            puts {
+                set xchan stdout
+                if {[llength $cmdArgs] > 1} {
+                    switch -- [lindex $cmdArgs 0] {
+                    -nonewline {
+                        if {[llength [lrange $cmdArgs 1 end]] > 1} {
+                            set xchan [lindex $cmdArgs 1]
+                        }
+                    }
+                    default {
+                        set xchan [lindex $cmdArgs 0]
+                    }
+                    }
+                }
+                
+                if {[regexp (stdout|stderr) $xchan]} {
+                    ::comm::comm send $id [uplevel subst $buffer]
+                }
+            }
+            }
+        }
+        }
+        
+        ::comm::comm send $id $script
+        ::comm::comm send $id {proc bgerror {args} {}}
     }
     
     ## \brief Setup the interp aliases for console mode
@@ -381,7 +465,6 @@ snit::widget ::Tmw::Console {
         if {[llength $args] > 3} {
             error "invalid arguments"
         }
-
         # for real __puts__ below
         set realArgs $args
         set newline "\n"
@@ -450,6 +533,9 @@ snit::widget ::Tmw::Console {
         slave {
             $SlaveInterp eval __gets__ $args
         }
+        comm {
+            comm::comm send $CommInterp [list eval gets $args]
+        }
         default {
             __gets__ {*}$args
         }
@@ -463,7 +549,7 @@ snit::widget ::Tmw::Console {
     method exitAlias {args} {
         $self SaveHistory
         switch -glob -- $options(-mode) {
-        slave {
+        slave - comm {
             interp delete $SlaveInterp
             interp create $SlaveInterp
             $self SetupAliases
@@ -515,9 +601,16 @@ snit::widget ::Tmw::Console {
         $textwin mark gravity limit left
     }
     
+    ## \brief If the -mode is comm and no comm id is given, ask for one
+    method AskForCommInterp {} {
+        $textwin fastinsert insert "Connect to Comm ID: " prompt
+        $textwin mark set limit insert
+        $textwin mark gravity limit left
+    }
+    
     ## \brief configure whether this console operates a slave interpreter
     method ConfigMode {opt val} {
-        set options($opt) $val
+        set options($opt) [lindex $val 0]
         
         # a proc for retrieval of all commands in namespaces
         # used for colorize
@@ -534,6 +627,10 @@ snit::widget ::Tmw::Console {
         slave {
             set SlaveInterp [interp create]
             $SlaveInterp eval $script
+        }
+        comm {
+            package require comm
+            set CommInterp [lindex $val 1]
         }
         default {
             if {$SlaveInterp != {}} {
@@ -647,17 +744,17 @@ proc Tmw::slaveconsole {path args} {
 package provide tmw::console 2.0
 
 #
-#Tmw::console .c -wrap none -font {"Lucida Sans Typewriter" 13} \
-#    -colors {
-#            Keywords {darkred normal}
-#            Braces {darkorange2 normal}
-#            Brackets {red normal}
-#            Parens {maroon4 normal}
-#            Options {darkorange3 normal}
-#            Digits {darkviolet normal}
-#            Strings {magenta normal}
-#            Vars {green4 normal}
-#            Comments {blue normal}
-#    } -vimode y -mode default
-#pack .c -expand y -fill both
+Tmw::console .c -wrap none -font {"Lucida Sans Typewriter" 13} \
+    -colors {
+            Keywords {darkred normal}
+            Braces {darkorange2 normal}
+            Brackets {red normal}
+            Parens {maroon4 normal}
+            Options {darkorange3 normal}
+            Digits {darkviolet normal}
+            Strings {magenta normal}
+            Vars {green4 normal}
+            Comments {blue normal}
+    } -vimode y -mode comm
+pack .c -expand y -fill both
 #
